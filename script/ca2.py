75f1f3c184b74f86f7f12048a3a804169c4ac945
tien <tianxsh@mail2.sysu.edu.cn>
2018-04-25 10:54:11 +0800

    add ca2 figure and script.

---
+++ script/ca2.py
@@ -0,0 +1,19 @@
+#! bin/python3
+# -*- encoding: utf8 -*-
+__author__ = 'Tian'
+import numpy as np
+import matplotlib.pyplot as plt
+
+light_voltage = np.array([2.3, 2.5, 2.8, 2.9])
+data = np.zeros(light_voltage.size)
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
+ax.set_xlabel("$U_{G2K}$")
+ax.set_ylabel("$I$")
+ax.set_title("U-I Graph")
+for j in light_voltage:
+    data = np.genfromtxt("../data/light_{0}.csv" .format(j), delimiter=',')
+    ax.plot(data[1:, 0] * 100, data[1:, 1] * 10**-7, label=r"$V_l={0}V$" .format(j), marker='^')
+
+plt.legend()
+plt.show(fig)
