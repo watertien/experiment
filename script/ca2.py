071f63ca691c8dd8a5c9d0df20b3068ecd909205
tien <tianxsh@mail2.sysu.edu.cn>
2018-04-30 22:37:09 +0800

    Maximum and minimum of the I-V graph from different voltage

--- script/ca2.py
+++ script/ca2.py
@@ -9,11 +9,11 @@ data = np.zeros(light_voltage.size)
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
 ax.set_xlabel("$U_{G2K}/V$", fontsize=12)
-ax.set_ylabel("$I/10^{-7}A$", fontsize=12)
+ax.set_ylabel("$I/10^{-10}A$", fontsize=12)
 ax.set_title("U-I Graph", fontsize=14)
 for j in light_voltage:
     data = np.genfromtxt("../data/light_{0}.csv" .format(j), delimiter=',')
-    ax.plot(data[1:, 0] * 100, data[1:, 1] , label=r"$V_l={0}V$" .format(j), marker='^')
+    ax.plot(data[1:, 0] * 100, data[1:, 1] * 10**3, label=r"$V_l={0}V$" .format(j), marker='^')
 
 plt.legend()
 plt.show(fig)
