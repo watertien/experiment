51396b26b4907159dce66a021d7ff2a52775c960
tien <tianxsh@mail2.sysu.edu.cn>
2018-06-07 20:22:24 +0800

    Complete CC1+ data acquisition, format data filename

--- script/cc1+.sil.py
+++ script/cc1+.sil.py
@@ -6,5 +6,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 T = np.arange(40, 110, 10)
-for i in T:
-    data = pd.read_excel("../data/196mm-{0}C-02.xlsx" .format(i), index_col=0)
+D = np.arange(150, 450, 50) - 45
+Tm = []
+Vm = []
+for i in D:
+    data = pd.read_excel("../data/{0}mm-100C-02sil.xlsx" .format(i), index_col=0)
+    Tm.append(data.iloc[:, 0].mean())
+    Vm.append(data.iloc[:, 2].mean())
+
+np.savetxt("../data/cc1+.distance-voltage.02sil.csv", (D, Vm), delimiter=',')
