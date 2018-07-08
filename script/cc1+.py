d953e83f5f2b948e99c9bbd9652347be4f12217b
Tian <86tianxs@gmail.com>
2018-07-08 10:40:16 +0800

    Add some figures for pre use

--- script/cc1+.py
+++ script/cc1+.py
@@ -5,10 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.dates as dates
-myFmt = dates.DateFormatter("%H:%M:%S")
+myFmt = dates.DateFormatter("%H:%M")
 
 
-data1 = pd.read_excel("../data/increase.xlsx", index_col=0)
+data1 = pd.read_excel("../data/decrease.xlsx", index_col=0)
 data1.columns = ["Temperature", "Actual Power", "Voltage", "Ni1000"]
 
 
@@ -22,9 +22,10 @@ ax.set_xlabel("Time", fontsize=14)
 
 ax1 = ax.twinx()
 ax1.set_ylabel("Power/W", fontsize=14)
-line2 = ax1.plot_date(data1.index, data1.iloc[:, 1], color="blue", marker="^", markersize=2, alpha=0.2, label="Supply Power")
+line2 = ax1.plot_date(data1.index, data1.iloc[:, 1], color="blue",
+                      marker="^", markersize=2, alpha=0.2, label="Supply Power")
 
 lines_set = [line1[0], line2[0]]
 ax.legend(lines_set, [i.get_label() for i in lines_set], prop={"size": 16})
 
-plt.show()
+plt.savefig("../figure/cc1+_pid_decrease.svg")
