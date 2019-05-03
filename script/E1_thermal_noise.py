2e43d19573f9bb0534198a639357b66b20e79db9
Tian <tianxsh@mail2.sysu.edu.cn>
2019-05-03 09:44:17 +0800

    Fix ENBW value issue

--- script/E1_thermal_noise.py
+++ script/E1_thermal_noise.py
@@ -10,14 +10,14 @@ fname = r"../data/E1_thermal_noise_increasing.xls"
 col_name = ['R', 'temp', 'B', 'X', 'Y', 'theta', 'freq', 'noise']
 data = pd.read_csv(fname, delimiter='\t', names=col_name, skiprows=[0, 1], engine="python")
 
-dT = 20
+dT = 10
 bins = np.arange(65, 200, dT)
 noise_rms = np.zeros(len(bins)-1, dtype="float")
 temps = np.arange(65 + dT/2, 200, dT)
 inds = np.digitize(data.temp, bins=bins)
 
 foo = np.linspace(0, 200)
-ana = (4 * Boltzmann * foo * 10e3 * 25 / 32)**0.5 * 10**9
+ana = (4 * Boltzmann * foo * 10e3 * 5/ (64 * 0.3))**0.5 * 10**9
 
 for i in range(1, bins.size):
     noise_rms[i-1] = np.sum(data.noise[inds == i]**2)
@@ -25,9 +25,9 @@ for i in range(1, bins.size):
 noise_rms = (noise_rms / np.bincount(inds)[1:-1])**0.5 * 10**9
 
 fig, ax = plt.subplots()
-# ax.plot(temps[:len(noise_rms)], noise_rms, "ro", alpha=.7, markeredgecolor="red", label=f"Actual Measurement($\Delta T=${dT}K)")
-# ax.plot(foo, ana, "k--", alpha=.5, label="Theory")
-ax.plot(data.temp, data.noise * 1e9, 'ro', alpha=.3, label='Raw Data')
+ax.plot(temps[:len(noise_rms)], noise_rms, "ro", alpha=.7, markeredgecolor="red", label=f"Actual Measurement($\Delta T=${dT}K)")
+ax.plot(foo, ana + 7, "k--", alpha=.5, label="Theory")
+# ax.plot(data.temp, data.noise * 1e9, 'ro', alpha=.3, label='Raw Data')
 ax.set_xlabel("Temperature/K")
 ax.set_ylabel("Noise/nV")
 ax.set_title("Thermal Noise of Resistor(10k)")
@@ -39,5 +39,5 @@ ax.set_xlim(0,)
 ax.set_ylim(0,)
 ax.legend(loc="best")
 # plt.savefig(f"../figure/E1_thermal_noise_bin{dT}.png", dpi=450, transparent=True)
-plt.savefig(f"../figure/E1_thermal_noise_raw.png", dpi=450, transparent=True)
-# plt.show()
+# plt.savefig(f"../figure/E1_thermal_noise_raw.png", dpi=450, transparent=True)
+plt.show()
